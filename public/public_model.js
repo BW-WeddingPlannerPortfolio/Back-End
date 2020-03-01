3718f5e4a197901f287ef6b8b53eef6bffa3690a
Schrese <53787633+Schrese@users.noreply.github.com>
2020-03-01 18:52:28 -0500

    can return a wedding by its id

--- public/public_model.js
+++ public/public_model.js
@@ -2,11 +2,16 @@ const db = require('../data/dbConfig.js');
 
 module.exports = {
     getWeddings,
+    getWeddingsById,
     getPlanners
 }
 
 function getWeddings() {
     return db('weddings');
+}
+
+function getWeddingsById(id) {
+    return db('weddings').where({ id })
 }
 
 function getPlanners() {
