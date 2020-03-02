9f0ab4dc7dbed00c098d639e39628772f265a123
Schrese <53787633+Schrese@users.noreply.github.com>
2020-03-01 19:12:05 -0500

    made auth middleware, realized that I need to add in planner files first

---
+++ auth/auth_middleware.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+const {jwtSecret} = require('../configs/secrets.js');
+
+module.exports = (req, res, next) => {
+    const token = req.headers.authorization;
+
+    if(token) {
+        jwt.verify(token, jwtSecret, (err, decodedToken) => {
+            if(err) {
+                console.log('error verifying token', err);
+                res.status(401).json({ message: 'could not verify credentials' })
+            } else {
+                req.user = decodedToken.user;
+                next();
+            }
+        })
+    } else {
+        res.status(401).json({ message: 'You don\'t have permission to be here' })
+    }
+
+}
