75ba189ca2dbe163866af0f923f358c729924877
Schrese <53787633+Schrese@users.noreply.github.com>
2020-03-02 19:54:41 -0500

    changed get to planner now includes array of weddings associated with the planner

--- public/public_model.js
+++ public/public_model.js
@@ -15,10 +15,22 @@ function getWeddingsById(id) {
     return db('weddings').where({ id })
 }
 
+async function getPlannerById(id) {
+    const planner = await db('planners as p').where({ id }).select('p.id', 'p.username', 'p.profile_pic', 'p.home_location', 'p.email').first();
+
+    const weddings = await db.from('weddings as w').where('w.planner_id', id)
+    
+    const profile = {
+        planner,
+        weddings: weddings
+    }
+    return profile;
+}
+
 function getPlanners() {
     return db('planners');
 }
 
-function getPlannerById(id) {
-    return db('planners').where({ id })
-}
+// function getPlannerById(id) {
+//     return db('planners').where({ id })
+// }
