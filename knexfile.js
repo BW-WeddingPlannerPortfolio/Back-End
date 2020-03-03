c25ed5cd8063465b34cc8935b19fc83f39980d3f
Schrese <53787633+Schrese@users.noreply.github.com>
2020-03-03 15:22:50 -0500

    Fixed knexfile, connection, not database

--- knexfile.js
+++ knexfile.js
@@ -21,27 +21,25 @@ module.exports = {
     },
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
 
   production: {
     client: 'pg',
-    connection: {
-      database: process.env.DATABASE_URL
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
